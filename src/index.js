2f50764e8cac1b82f88788dd3eba8bc54fdd320e
Yatira <irishovna@gmail.com>
2022-02-23 10:05:06 +0300

    docs: add structure of the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+            return (
+          parseFloat(
+            n
+              .toString()
+              .split('')
+              .reverse()
+              .join('')
+          ) * Math.sign(n)
+        )                 
+      
 }
